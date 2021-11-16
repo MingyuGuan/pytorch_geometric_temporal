ae940f39b6ca595241dbff48d16eea2c51b8ea48
Mingyu Guan <mingyu@mingyus-mbp.lan>
2021-11-16 16:02:28 -0500

    put data to device

--- examples/reuse/gcnconvlstm_example.py
+++ examples/reuse/gcnconvlstm_example.py
@@ -16,9 +16,6 @@ dataset = loader.get_dataset()
 
 train_dataset, test_dataset = temporal_signal_split(dataset, train_ratio=0.2)
 
-train_dataset.to(device)
-test_dataset.to(device)
-
 class RecurrentGCN(torch.nn.Module):
     def __init__(self, node_features):
         super(RecurrentGCN, self).__init__()
@@ -41,6 +38,7 @@ for epoch in tqdm(range(5)):
     cost = 0
     h, c = None, None
     for time, snapshot in enumerate(train_dataset):
+        snapshot.to(device)
         y_hat, h, c = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr, h, c)
         cost = cost + torch.mean((y_hat-snapshot.y)**2)
     cost = cost / (time+1)
