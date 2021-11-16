0581992ba719a54c48771f4a1b6ad32a1c53d9ea
Mingyu Guan <mingyu@mingyus-mbp.lan>
2021-11-16 18:57:17 -0500

    use chebconv

--- examples/reuse/chebconvlstm_example.py
+++ examples/reuse/chebconvlstm_example.py
@@ -2,7 +2,7 @@ from tqdm import tqdm
 
 import torch
 import torch.nn.functional as F
-from temporal_gnns import GCNConvLSTM
+from temporal_gnns import ChebConvLSTM
 
 from torch_geometric_temporal.dataset import ChickenpoxDatasetLoader, WindmillOutputSmallDatasetLoader,WindmillOutputMediumDatasetLoader,WindmillOutputLargeDatasetLoader
 from torch_geometric_temporal.signal import temporal_signal_split
@@ -19,7 +19,7 @@ train_dataset, test_dataset = temporal_signal_split(dataset, train_ratio=0.2)
 class RecurrentGCN(torch.nn.Module):
     def __init__(self, node_features):
         super(RecurrentGCN, self).__init__()
-        self.recurrent = GCNConvLSTM(node_features, 32)
+        self.recurrent = ChebConvLSTM(node_features, 32, 1)
         self.linear = torch.nn.Linear(32, 1)
 
     def forward(self, x, edge_index, edge_weight, h, c):
