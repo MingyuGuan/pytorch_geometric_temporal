2ed64f1efa8df50d45738ed21f833b37869700c9
Mingyu Guan <mingyu@mingyus-mbp.lan>
2021-11-22 16:37:27 -0500

    debug - replicate nodes

--- examples/reuse/chebconvgru_example.py
+++ examples/reuse/chebconvgru_example.py
@@ -63,7 +63,7 @@ for epoch in tqdm(range(args.epochs)):
     cost = 0
     for time, snapshot in enumerate(train_dataset):
         if args.rep > 1:
-            snapshot = Batch.from_data_list([Data(x=snapshot.x, edge_index=snapshot.edge_index, edge_attr=snapshot.edge_attr) for i in range(args.rep)])
+            snapshot = Batch.from_data_list([Data(x=snapshot.x, edge_index=snapshot.edge_index, edge_attr=snapshot.edge_attr, y=snapshot.y) for i in range(args.rep)])
         snapshot.to(device)
         y_hat = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)
         cost = cost + torch.mean((y_hat-snapshot.y)**2)
